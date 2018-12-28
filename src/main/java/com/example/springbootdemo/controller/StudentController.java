ebc1fa3655e92024c1bdb76ccd104dbe3d322555
zifeng.deng <522243062@qq.com>
2018-12-28 17:52:05 +0800

    student类增删改查的简单实现

--- src/main/java/com/example/springbootdemo/controller/StudentController.java
+++ src/main/java/com/example/springbootdemo/controller/StudentController.java
@@ -8,37 +8,71 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Log
 @Controller
-@RequestMapping("/student")
+@RequestMapping("/stu")
 public class StudentController {
 
     @Autowired
     StudentService studentService;
 
     @GetMapping(value = "/get/{id}")
-    public Student getStudent(@PathVariable("id") int id, Model model){
+    public String getStudent(@PathVariable("id") int id, Model model){
         //调用service根据id获取student
         Student student = studentService.getStudentById(id);
         model.addAttribute("student", student);
-        return student;
+        return "add";
     }
 
-    @GetMapping(value = "/delete/{id}")
-    public void deleteStudent(@PathVariable("id") int id) {
-        studentService.deleteStudentById(id);
-    }
-
-    @PostMapping(value = "/add")
-    public String addAndUpdateStudent(@ModelAttribute("student") Student student,Model model){
-        studentService.saveStudent(student);
+    @GetMapping(value = "/getAll")
+    public String getStudents(Model model) {
+        List<Student> students = studentService.getAllStudent();
+        //有数据才把students传给页面
+        if(students.size()>0) {
+            model.addAttribute("students", students);
+        }
         return "index";
     }
 
-    @PostMapping(value = "/update")
-    public void updateStudent(@RequestBody(required = false) Student student){
-        studentService.updateStudent(student);
+    @GetMapping(value = "/delete/{id}")
+    public String deleteStudent(@PathVariable("id") int id) {
+        studentService.deleteStudentById(id);
+        return "redirect:/stu/getAll";
     }
+
+    @PostMapping(value = "/add")
+    public String addStudent(Student student,Model model){
+        studentService.saveStudent(student);
+        //把学生列表传给index作显示
+        model.addAttribute("students", studentService.getAllStudent());
+        return "redirect:/stu/getAll";
+    }
+
+    @GetMapping(value = "/add")
+    public String addStudent(Model model){
+        model.addAttribute("student", new Student());
+        model.addAttribute("action", "add");
+        return "student/studentForm";
+    }
+
+    @GetMapping(value = "/update/{id}")
+    public String updateStudentForm(@PathVariable("id") int id, Model model){
+        Student student = studentService.getStudentById(id);
+        model.addAttribute("student", student);
+        model.addAttribute("action", "update");
+        return "student/studentForm";
+    }
+
+    @PostMapping(value = "/update")
+    public String updateStudent(@ModelAttribute Student student, Model model){
+        studentService.updateStudent(student);
+
+
+        return "redirect:/stu/getAll";
+    }
+
 }
 
 
